11ee929ee98b89982de5efe88e8848ea228c4170
loki <loki@wondermove.net>
2022-08-27 23:32:08 +0900

    0.0.12

--- src/index.js
+++ src/index.js
@@ -1,2 +1,2 @@
-export * from 'src/@core/components/spinner';
-export { default as Spinner } from 'src/@core/components/spinner';
+export * from 'src/@core/components/spinner/index';
+export { default as Spinner } from 'src/@core/components/spinner/index';
